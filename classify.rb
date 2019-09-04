2a526c060e677e5ba8021fb63a3af39f6ab43b4b
Kevin McHugh <kev@kevinmchugh.me>
2019-09-04 10:48:07 -0500

    Commits a couple classify changes

--- classify.rb
+++ classify.rb
@@ -1,3 +1,4 @@
+require 'pp'
 class TaggableFile
   attr_reader :file_name, :root_dir
   def initialize(file_name, root_dir)
@@ -22,6 +23,8 @@ class Tag
     new_file_path = "#{full_path}/#{taggable_file.file_name}"
     File.symlink(taggable_file.full_path, new_file_path) unless File.exists?(new_file_path)
   end
+  def to_s; name; end
+  def pp; name; end
 
   private
   def ensure_folder
@@ -38,17 +41,24 @@ existing_tags = Dir.new(root_dir).children.find_all { |child| Dir.exist?("#{root
 tags = existing_tags.map { |t| Tag.new(t, root_dir) }
 
 tags_by_index = {}
-tags.each_with_index { |tag,i| tags_by_index[i + 1] = tag }
+tags.sort_by { |t| t.name.downcase }.each_with_index { |tag,i| tags_by_index[i + 1] = tag }
+reached_starting_point = false
 
-Dir.new(root_dir).children.each do |child|
+starting_point = ENV['STARTING_POINT']
+
+Dir.new(root_dir).children.sort.each do |child|
+  next unless child.downcase.end_with?('jpg') || child.downcase.end_with?('png')
+  reached_starting_point = child == starting_point unless reached_starting_point
+  next unless reached_starting_point
+
   file = TaggableFile.new(child, root_dir)
   `open '#{file.full_path}'`
-  puts tags_by_index
+  pp tags_by_index.map { |i,t| "#{i}:#{t.name}"}
   puts "enter tag indices separated by commas"
+  puts child
   input_tags = gets.strip
   input_tags.split(',').map(&:to_i).reject(&:zero?).each do |index|
     tag = tags_by_index[index]
     tag.tag_file(file)
   end
-  raise "just testing."
 end
